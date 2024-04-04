1450aaeeb120064a54b02402843ed18d91b87748
Matheus Emanoel <matheusemanoel1407@gmail.com>
2024-04-04 17:18:36 -0400

    bug fix de assinatura renderizar somente atualizando a tela resolvido

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -10,9 +10,9 @@ import Draggable from 'react-draggable';
 
 
 
-export  function DragOnDropFilesRender({signatures, pdfFile}){
-  console.log('Assinaturas recebidas:', signatures);
-  console.log('Arquivo pdf:', pdfFile);
+export function DragOnDropFilesRender({signatures, pdfFile}){
+console.log('Assinaturas recebidas:', signatures);
+console.log('Arquivo pdf:', pdfFile);
 return(
 <>
 
@@ -41,36 +41,42 @@ const DropZoneComponent = ({ signatures, pdfFile }) => {
     initializeAllSignatures();
   }, [numPages]);
 
-  const toggleSignatureFix = (pageNumber, signatureIndex) => {
+  useEffect(() => {
+    if (signatures && Object.keys(allSignatures).length > 0) {
+      setAllSignatures((prevSignatures) => {
+        const updatedSignatures = { ...prevSignatures };
+        Object.keys(updatedSignatures).forEach((pageNumber) => {
+          updatedSignatures[pageNumber] = signatures.map((sig) => {
+            const existingSignature = updatedSignatures[pageNumber].find((s) => s.id === sig.id);
+            return existingSignature ? existingSignature : { ...sig, fixed: false };
+          });
+        });
+        return updatedSignatures;
+      });
+    }
+  }, [signatures]);
+
+  const toggleSignatureFix = useCallback((pageNumber, signatureIndex) => {
     setAllSignatures((prevSignatures) => {
       const updatedSignatures = { ...prevSignatures };
       if (!updatedSignatures[pageNumber]) {
         updatedSignatures[pageNumber] = signatures.map((sig) => ({ ...sig, fixed: false }));
       }
-      updatedSignatures[pageNumber][signatureIndex] = {
-        ...updatedSignatures[pageNumber][signatureIndex],
-        fixed: !updatedSignatures[pageNumber][signatureIndex].fixed,
-      };
+      updatedSignatures[pageNumber][signatureIndex].fixed = !updatedSignatures[pageNumber][signatureIndex].fixed;
       return updatedSignatures;
     });
-  };
+  }, [signatures]);
 
-  const handleDragStop = (pageNumber, e, data, signatureIndex) => {
+  const handleDragStop = useCallback((pageNumber, _, data, signatureIndex) => {
     if (!allSignatures[pageNumber][signatureIndex].fixed) {
-      const newPosition = {
-        x: data.x,
-        y: data.y,
-      };
+      const newPosition = { x: data.x, y: data.y };
       setAllSignatures((prevSignatures) => {
         const updatedSignatures = { ...prevSignatures };
-        updatedSignatures[pageNumber][signatureIndex] = {
-          ...updatedSignatures[pageNumber][signatureIndex],
-          ...newPosition,
-        };
+        updatedSignatures[pageNumber][signatureIndex] = { ...updatedSignatures[pageNumber][signatureIndex], ...newPosition };
         return updatedSignatures;
       });
     }
-  };
+  }, [allSignatures]);
 
   const handleNextPage = () => {
     if (pageNumber < numPages) {
@@ -87,7 +93,7 @@ const DropZoneComponent = ({ signatures, pdfFile }) => {
   };
 
   const onDrop = useCallback((acceptedFiles) => {
-    setFiles(acceptedFiles);
+    setFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
     setShowPDF(true);
     setNumPages(acceptedFiles.length);
   }, []);
@@ -105,13 +111,16 @@ const DropZoneComponent = ({ signatures, pdfFile }) => {
     setAllSignatures(initialSignatures);
   };
 
-  const addSignatureToCurrentPage = (pageNumber) => {
-    setAllSignatures((prevSignatures) => {
-      const updatedSignatures = { ...prevSignatures };
-      updatedSignatures[pageNumber] = [...(updatedSignatures[pageNumber] || [])];
-      return updatedSignatures;
-    });
-  };
+  const addSignatureToCurrentPage = useCallback(
+    (pageNumberToAdd) => {
+      setAllSignatures((prevSignatures) => {
+        const updatedSignatures = { ...prevSignatures };
+        updatedSignatures[pageNumberToAdd] = updatedSignatures[pageNumberToAdd] || [];
+        return updatedSignatures;
+      });
+    },
+    [setAllSignatures]
+  );
 
   return (
     <div>
@@ -121,12 +130,6 @@ const DropZoneComponent = ({ signatures, pdfFile }) => {
         border-gray-400 border-dashed rounded-lg cursor-pointer ${isDragActive ? 'bg-gray-100' : ''}`}
       >
         <input {...getInputProps()} />
-        {showPDF && (
-          <div className="w-full h-2 mb-4 overflow-hidden bg-gray-200 rounded-md">
-            <div className="h-full bg-blue-500" style={{ width: `${numPages}%` }}></div>
-          </div>
-        )}
-        {showPDF && <p className="text-gray-600">{`Upload em progresso: ${numPages}%`}</p>}
         <p className="text-gray-600">
           {isDragActive ? 'Solte os arquivos aqui' : 'Arraste e solte seus arquivos aqui ou clique para selecionar'}
         </p>
@@ -140,38 +143,30 @@ const DropZoneComponent = ({ signatures, pdfFile }) => {
         {showPDF && (
           <div className="pdf-container" ref={containerRef}>
             <Document file={pdfFile} onLoadSuccess={({ numPages }) => setNumPages(numPages)}>
-              {Array.from(new Array(numPages), (el, pageIndex) => (
+              {Array.from(new Array(numPages), (_, pageIndex) => (
                 <div key={`page_${pageIndex + 1}`}>
                   {pageIndex + 1 === pageNumber && (
-                    <div className='relative'>
-
-{allSignatures[pageNumber] &&
-                        allSignatures[pageNumber].map((signature, index) => (
-                          <Draggable
-                            key={`signature_${index}`}
-                            defaultPosition={{ x: signature.x, y: signature.y }}
-                            onStop={(e, data) => handleDragStop(pageNumber, e, data, index)}
-                            disabled={signature.fixed}
-                          >
-                            <div
-                              className={`absolute z-50 p-2 flex flex-col  bg-white/50 border-2 border-dashed border-gray-300 rounded shadow-md cursor-pointer ${
-                                signature.fixed ? 'opacity-50' : ''
-                              }`}
+                    <div className="relative">
+                      {allSignatures[pageNumber] &&
+                        allSignatures[pageNumber].map((signature, index) => {
+                          console.log(`Posição: x=${signature.x}, y=${signature.y}, Página: ${pageNumber}`);
+                          return (
+                            <Draggable
+                              key={`signature_${index}`}
+                              defaultPosition={{ x: signature.x, y: signature.y }}
+                              onStop={(e, data) => handleDragStop(pageNumber, e, data, index)}
+                              disabled={signature.fixed}
                             >
-                              <div className="flex justify-end">
-                                <button
-                                  onClick={() => toggleSignatureFix(pageNumber, index)}
-                                  className="ml-2 text-xs text-blue-500"
-                                >
-                                  {signature.fixed ? 'Desafixar' : 'Fixar'}
-                                </button>
+                              <div
+                                className={`absolute z-50 p-2 flex flex-col bg-white/50 border-2 border-dashed border-gray-300
+                                rounded shadow-md cursor-pointer ${signature.fixed ? 'opacity-50' : ''}`}
+                              >
+                                <span className="font-bold">{signature.name}</span> <span>{signature.email}</span>
                               </div>
-                              <span className="font-bold">{signature.name}</span> <span>{signature.email}</span>
-                            </div>
-                          </Draggable>
-                        ))}
+                            </Draggable>
+                          );
+                        })}
                       <Page className="z-10" renderTextLayer={false} pageNumber={pageNumber} />
-                     
                     </div>
                   )}
                 </div>
@@ -196,7 +191,7 @@ const DropZoneComponent = ({ signatures, pdfFile }) => {
             onClick={handleNextPage}
             disabled={pageNumber >= numPages}
             className={`px-2 py-2 w-40 h-12 rounded-md ${
-              pageNumber >= numPages ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-blue-500 text-white hover:bg-blue-600'
+              pageNumber >= numPages ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-blue-500 text-white  hover:bg-blue-600'
             }`}
           >
             Próxima Página
